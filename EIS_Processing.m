2937d106cf38638ed39bcbcda381d14283b0c84a
mmarquesEE <mateus.marques@ee.ufcg.edu.br>
2024-03-27 12:05:44 -0300

    updated eis

--- EIS_Processing.m
+++ EIS_Processing.m
@@ -1,12 +1,18 @@
 close all
+% 
+% a = readtable("EIS.csv")
+% figure
+% plot3(a.Real_m__,a.Imaginary,a.Frequency_Hz_)
 
-a = readtable("EIS.csv")
-figure
-plot(a.Real_m__,a.Imaginary,'o')
+b = readtable("SingleFreq5e-1.csv")
+R = b.Real(b.Imaginary>0 & b.Imaginary<5);
+I = b.Imaginary(b.Imaginary>0&b.Imaginary<5);
+plot(R,I,mean(R),mean(I),'o')
+[mean(R),mean(I)]
 
-b = readtable("DCL.csv")
-figure
-plot(b.Voltage)
-
-figure
-plot(b.Current)
+% b = readtable("DCL.csv")
+% figure
+% plot(b.Voltage)
+% 
+% figure
+% plot(b.Current)
--- eis/eis.py
+++ eis/eis.py
@@ -12,13 +12,17 @@ def get_data_as_df(graph_data):
                 value = float(graph_data.InfoPaneValues[field_index])
             except:
                 value = graph_data.InfoPaneValues[field_index]
-
+                idx = int(value.split("/")[0].strip()) - 1
+            
             dict_[graph_data.InfoPaneNames[field_index]] = value
     
-    return pd.DataFrame(data=dict_,index=[0])
+    return pd.DataFrame(data=dict_,index=[idx])
 
 class NoConnectionException(Exception):
     """The device is not connected! Verify the COM port"""
+
+class FailedMeasureException(Exception):
+    """Failed to Measure"""
 
 class EIS:
     def __init__(
@@ -51,34 +55,36 @@ class EIS:
 
         self.sensorpal = SensorPalAPI()
 
+        self.technique = "Battery Impedance"
+        self.technique_parameters = self.sensorpal.GetDefaultTechniqueParameters(self.technique)
+
+        self.reset(init_call=True)
+    
+    def reset(self,init_call=False):
+        if not init_call:
+            self.sensorpal.CloseConnection()
+            self.connect(self.com_port)
+            self.set_forced_reset_params()
+
+            try:
+                self.run(reset_run=True)
+            except Exception as e:
+                print(e)
+                raise e
+           
+            self.sensorpal.CloseConnection()
+        
+        self.connect(self.com_port)
+        self.set_self_params()
+
+    def connect(self,com_port):
         try:
             self.sensorpal.OpenConnection(com_port)
         except Exception as e:
             print(e)
             print("I was unable to open '%s'. Is it connected?" % com_port)
-        else:
-            self.technique = "Battery Impedance"
-            self.technique_parameters = self.sensorpal.GetDefaultTechniqueParameters(self.technique)
 
-            self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
-                "Start Frequency", self.start_frequency)
-            self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
-                "Stop Frequency", self.stop_frequency)
-            self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
-                "Points", self.points)
-            self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
-                "DC Bias", self.dc_bias)
-            self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
-                "AC Amplitude", self.ac_amplitude)
-            self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
-                "Logarithmic", self.logarithmic)
-            self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
-                "Calibration Resistor", self.calibration_resistor)
-            
-            self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
-                "Enable", "1")
-    
-    def run(self, filename="EIS.csv"):
+    def run(self, filename=None,reset_after=False,reset_run=False):
         try:
             self.sensorpal.Measure(self.technique, self.technique_parameters)
         except Exception as e:
@@ -92,7 +98,60 @@ class EIS:
                     dfi.append(get_data_as_df(graph_data))
                     
             self.sensorpal.CloseConnection()
-            df = pd.concat(dfi,ignore_index=True)
-            df.to_csv(filename)
+            
+            if not reset_run:
+                df = pd.concat(dfi)
+                
+                points = int(self.points)
+                points = points if points > 0 else 100
 
-            return df
+                if self.logarithmic == "1":
+                    freqs = np.logspace(
+                        np.log10(float(self.start_frequency)),
+                        np.log10(float(self.stop_frequency)),
+                        points
+                    )
+                else:
+                    freqs = np.linspace(
+                        float(self.start_frequency),
+                        float(self.stop_frequency),
+                        points
+                    )
+                
+                df["Frequency (Hz)"] = freqs[df.index]
+
+                if filename != None:
+                    df.to_csv(filename)
+
+                if reset_after:
+                    self.reset()
+
+                return df
+    
+    def set_forced_reset_params(self):
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "Start Frequency", "200000")
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "Stop Frequency", "200000")
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "Points", "10")
+    
+    def set_self_params(self):
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "Start Frequency", self.start_frequency)
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "Stop Frequency", self.stop_frequency)
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "Points", self.points)
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "DC Bias", self.dc_bias)
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "AC Amplitude", self.ac_amplitude)
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "Logarithmic", self.logarithmic)
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "Calibration Resistor", self.calibration_resistor)
+        
+        self.sensorpal.UpdateTechniqueParameter(self.technique, self.technique_parameters,
+            "Enable", "1")
+        
